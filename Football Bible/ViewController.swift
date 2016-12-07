4d9c2a23b83b273b24a3ccdb556344afa6f0a74c
Christopher Ras <chris@macbook-pro.local>
2016-12-07 23:44:44 +0100

    Initial Commit

---
+++ Football Bible/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Football Bible
+//
+//  Created by Christopher Ras on 07/12/2016.
+//  Copyright © 2016 Chris Ras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
