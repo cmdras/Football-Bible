37dd9f83da6cf68aa410b75592e3fe3e41f11e01
Christopher Ras <chris@macbook-pro.local>
2016-12-09 03:48:19 +0100

    Added tableView to main screen

--- Football Bible/ViewController.swift
+++ Football Bible/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var teamTable: UITableView!
+    
+    let footballTeams = ["Ajax Amsterdam", "Man Utd", "Barcelona" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return footballTeams.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = teamTable.dequeueReusableCell(withIdentifier: "teamCell", for: indexPath)
+            as! teamCell
+        
+        cell.teamName.text = footballTeams[indexPath.row]
+        return cell
+        
     }
 
 
